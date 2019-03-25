209decf543572bb81215a1a23816c60319de3f6f
JackTanRoo <jack.tan.lu@hotmail.com>
2019-03-24 18:07:11 -0700

    added ccxt and binance support

--- server.js
+++ server.js
@@ -14,11 +14,14 @@
 // # calculate profitability of every trade
 
 
+// // # select crypto to track to track pricing data and make trades
+
+
 const WebSocket = require('ws');
 const express = require('express');
 var path = require('path');
-
-
+var ccxt = require("ccxt");
+// console.log("ccxt")
 // init context of params
 
 context = {
@@ -84,30 +87,41 @@ wss.on('connection', function connection(socket) {
 });
 
 
-// establish connection with coinjar
+// establish connection with coinjar and pull data
 
 var coinjarWss = new WebSocket(context["crypto_exchange_parameters"]["coinjar"]["data_endpoint"])
 
-coinjarWss.on("open", function connection(socket){
-	console.log("I am have connected to coinjar")	
+// coinjarWss.on("open", function connection(socket){
+// 	console.log("I am have connected to coinjar")	
 
-	// set heartbeat every 40 seconds - coinjar requires every 45 seconds
-	setInterval(function(){
-		coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["heartbeat_message"]);
-		console.log("sending heart beat!")
-	}, context["crypto_exchange_parameters"]["coinjar"]["heartbeat_freq"])
+// 	// set heartbeat every 40 seconds - coinjar requires every 45 seconds
+// 	setInterval(function(){
+// 		coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["heartbeat_message"]);
+// 		console.log("sending heart beat!")
+// 	}, context["crypto_exchange_parameters"]["coinjar"]["heartbeat_freq"])
 
-	// get message from coinjar Socket
+// 	// get message from coinjar Socket
 
-	coinjarWss.on('message', function incoming(message) {
+// 	coinjarWss.on('message', function incoming(message) {
 
-		// coinjarWss.clients.forEach(client => {
-	 //      client.send(message);
-		// });  
-		console.log("received from a client: ", message)
-	});  
+// 		// coinjarWss.clients.forEach(client => {
+// 	 //      client.send(message);
+// 		// });  
+// 		console.log("received from a client: ", message)
+// 	});  
 
-	coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]);
+// 	coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]);
 
-})
+// })
+
+// establish connection with binance
+
+var exchangeObjOne = new ccxt[context.selected_exchanges.exchange_1]()
+
+console.log("I am binance", exchangeObjOne);
+
+
+
+
+
 
