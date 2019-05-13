1c46925d43965c0f12c4bcad8aee9e17a109e893
JackTanRoo <jack.tan.lu@hotmail.com>
2019-05-12 19:37:51 -0700

    fixing coinjar heartbeat problem

--- test.js
+++ test.js
@@ -4,163 +4,248 @@ var path = require('path');
 var app = express();
 var server = require('http').createServer(app);
 
+const WebSocket = require('ws');
 
-// app.use('/', express.static(path.join(__dirname, '/index-backup.html')));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index-backup.html'));
-});
+var context = {
+    "selected_exchanges": {
+        "exchange_1": "binance",
+        "exchange_2": "coinjar",    
+    },
+    "crypto_exchange_parameters": {
+        "coinjar": {
+            "name": "coinjar",
+            "data_endpoint": "wss://feed.exchange.coinjar.com/socket/websocket",
+            "heartbeat_freq": 15000,
+            "heartbeat_message": { "topic": "phoenix", "event": "heartbeat", "payload": {}, "ref": 0 },
+            "channel_sub": {
+                "BTCAUD": '{ "topic": "trades:BTCAUD", "event": "phx_join", "payload": {}, "ref": 0 }',
+                "LTCAUD": '{ "topic": "trades:LTCAUD", "event": "phx_join", "payload": {}, "ref": 0 }',
+                "ZECBTC": '{ "topic": "trades:ZECBTC", "event": "phx_join", "payload": {}, "ref": 0 }'
+            }, 
+            "slippage": 0.01,
+            "fees": 0.001,
+            "current_fiat": 5000,
+            "current_crypto": 100
+        },
+        "binance" :{
+            "name": "binance",
+            "data_endpoint": "wss://stream.binance.com:9443",
+            "heartbeat_freq": 3 * 60 * 1000,
+            "channel_sub": {
+                "BTCUSDT": "BTCUSDT",
+                "ZECBTC": "ZECBTC",
+                "LTCUSDT": "LTCUSDT"
+            },
+            "slippage": 0.005,
+            "fees": 0.001,
+            "current_fiat": 5000,
+            "currentLTCAUD_crypto": 100,
+            "time_sync_adjustment" : (8 * 60 * 60 + 19 * 60 + 55)
+        }
+    },
+    "forex_parameters":{
+        "forex_api": "https://api.exchangeratesapi.io/latest?base=AUD"
+    },
+    "selected_trading_pairs": {
+        "crypto_1": "LTC/USDT",
+        "crypto_2": "LTC/AUD",
+        "fiat_1": "AUD",
+        "fiat_2": "USD"
+    },
+    "marginOfError": 0.01,
+    "amountToTrade": 0.05,
+    "trading_data": {
+        "binance": {
+            "BTCUSDT" : 
+            [
+                {
+                symbol: "BTCUSDT",
+                time: Date.now(),
+                price: 7300,
+                quantity: 1,
+                exchange: "binance" 
+                }
+            ],
+            "ZECBTC" : [
+                {
+                symbol: "ZECBTC",
+                time: Date.now(),
+                price: 0.009,
+                quantity: 1,
+                exchange: "binance" 
+                }
+            ],
+            "LTCUSDT" : [
+                {
+                symbol: "LTCUSDT",
+                time: Date.now(),
+                price: 85,
+                quantity: 1,
+                exchange: "binance" 
+                }
+            ] 
+        },
+        "coinjar": {
+            "BTCAUD" : 
+            [
+                {
+                symbol: "BTCAUD",
+                time: Date.now(),
+                price: 9900,
+                quantity: 1,
+                exchange: "coinjar" 
+                }
+            ],
+            "ZECBTC" : [
+                {
+                symbol: "ZECBTC",
+                time: Date.now(),
+                price: 0.009,
+                quantity: 1,
+                exchange: "coinjar" 
+                }
+            ],
+            "LTCAUD" : [
+                {
+                symbol: "LTCAUD",
+                time: Date.now(),
+                price: 121,
+                quantity: 1,
+                exchange: "coinjar" 
+                }
+            ] 
+        },
+        "forex":{
+            "AUDUSD" : []
+        },
+    },
+    "twoWayLookup" :{
+        BTCUSDT : "BTCAUD",
+        LTCUSDT : "LTCAUD",
+        ZECBTC : "ZECBTC", 
+        BTCAUD : "BTCUSDT",
+        LTCAUD : "LTCUSDT",         
+    },
+    "arbitrage_opportunities":{
+        selected : []
+            // 150230214310 : {
 
-app.use(express.static('./'))
+            // }
+        ,
+        allOpportunities : []
+            // 12325315314 : {
 
-var io = require('socket.io')(server);
+            // }
+        
+        // selected : {
+        //  // trade_id: belo
+        // },
+        // allOpportunities: {
+
+        
+        // 15105104313 : {
+        //  // {
+        // selected: true or false
+        // trade_id: 1
+        // type_of_trade: twoWay or threeWay,
+        // time_of_trade: trades.first.time,
+        // ROI_of_trade: ROI
+        // trades: {
+            // first: {
+            //  exchange: binance
+            //  symbol: BTCUSDT
+            //  indexTradeData: Index in the Trade Data Array
+            //  trade: buy
+            //  time: time
+            //  buyAsset: BTC
+            //  sellAsset: USDT
+            //  buyPrice: BTC/USDT Price
+            //  buyQuantity: quantity of buyAsset bought
+            //  sellQuantity: quantity of sellAsset sold
+            // },
+            // second: {
+            //  exchange: binance
+            //  symbol: BTCUSDT
+            //  indexTradeData: Index in the Trade Data Array
+            //  trade: buy
+            //  time: time
+            //  buyAsset: BTC
+            //  sellAsset: USDT
+            //  buyPrice: BTC/USDT Price
+            //  buyQuantity: quantity of buyAsset bought
+            //  sellQuantity: quantity of sellAsset sold
+            // },
+            // third: {
+            //  exchange: binance
+            //  symbol: BTCUSDT
+            //  indexTradeData: Index in the Trade Data Array
+            //  trade: buy
+            //  time: time
+            //  buyAsset: BTC
+            //  sellAsset: USDT
+            //  buyPrice: BTC/USDT Price
+            //  buyQuantity: quantity of buyAsset bought
+            //  sellQuantity: quantity of sellAsset sold
+            // },
+        // }
+        // exchange_first: exchangeName,
+        // time_first: exchangeNameTime,
+        // time_second: 
+    // }
+        // }
+    }   
+};
+
+var coinjarWss = new WebSocket(context["crypto_exchange_parameters"]["coinjar"]["data_endpoint"]);
+
+coinjarWss.on("open", function connection(socket){
+    console.log("Server connected to coinjar")  
+
+    // coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]["BTCUSDT"]);
+    // coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]["LTCAUD"]);
+    // coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]["ZECBTC"]);
+
+    // set heartbeat every 40 seconds - coinjar requires every 45 seconds
+    setInterval(function(){
+        console.log("trying to send a heartbeat")
+        coinjarWss.send(JSON.stringify(context["crypto_exchange_parameters"]["coinjar"]["heartbeat_message"]));
+        // console.log("sending heart beat!")
+    }, context["crypto_exchange_parameters"]["coinjar"]["heartbeat_freq"])
 
 
-io.on('connection', function(client) {
+    // subscribe to coinjar token channel
+    // coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]["BTCUSD"]);
+    // coinjarWss.send(context["crypto_exchange_parameters"]["coinjar"]["channel_sub"]["ZECUSD"]);
 
-    client.on('disconnect', function() {
-	    console.log("disconnected")
+
+    // get message from coinjar Socket
+
+    coinjarWss.on('message', function incoming(message) {
+        // console.log("received message", message)
+        coinjarDataObj = JSON.parse(message);
+        console.log(coinjarDataObj)
+        coinjarData = coinjarDataObj["payload"];
+        
+        if (coinjarDataObj["topic"] !== "phoenix") {
+            if (coinjarDataObj["event"] == "init") {
+                for (var i = 0; i < coinjarDataObj.payload.trades.length; i ++){                    
+                    context.trading_data = updateTradingLog(context.trading_data, "coinjar", coinjarDataObj.topic.substr(7, coinjarDataObj.topic.length-7), coinjarDataObj.payload.trades[i])
+                }
+            } else if (coinjarDataObj["event"] == "new") {
+                console.log("IAM DATA OBJ", coinjarDataObj.payload.trades[0])
+                context.trading_data = updateTradingLog(context.trading_data, "coinjar", coinjarDataObj.topic.substr(7, coinjarDataObj.topic.length-7), coinjarDataObj.payload.trades[0])
+            }
+        }
     });
 
 
-    client.on('room', function(data) {
-        client.join(data.roomId);
-        console.log(' Client joined the room and client id is '+ client.id);
-
+    coinjarWss.on('close', function (evt) {
+        console.log("closed")
     });
-    
-    
-    client.on('toBackEnd', function(data) {
-    	console.log("in backend", data, "received ")
-        client.in(data.roomId).emit('message', "Server received stuff in toBackend, please display");
-    })
 
-    client.emit("message", "hahahaha")
+    coinjarWss.on('error', function (evt) {
+        console.log("error!!", evt)
+    });
+
 
 });
-
-server.listen(3000, function () {
-    console.log('node.js static server listening on port: ' + 3000 + ", fdafdafaw with websockets listener")
-});
-
-
-
-
-
-
-
-// const WebSocket = require('ws');
-// const express = require('express');
-// var path = require('path');
-// var ccxt = require("ccxt");
-// const axios = require('axios');
-// var moment = require("moment");
-// var binanceKey = require("./binance-key");
-
-
-
-// var app = express();
-
-// //init Express Router
-// var router = express.Router();
-// var port = process.env.PORT || 3000;
-
-// //return static page with websocket client
-
-// app.use(express.static('./'))
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-// });
-
-// var server = app.listen(port, function () {
-//     console.log('node.js static server listening on port: ' + port + ", with websockets listener")
-// })
-
-// const wss = new WebSocket.Server({ server });
-
-// wss.on('connection', ws => {
-// 	console.log("connected!")
-//   ws.on('message', message => {
-//     console.log(`Received message => ${message}`)
-//   })
-//   ws.on("error", function(err){
-//   	console.error("error", err)
-//   })
-//   ws.send('Hello! Message From Server!!')
-// })
-
-
-
-// wss.on('open', function open() {
-//   console.log("OPENED");
-//   wss.send('opened, something');
-// });
-
-// wss.on('connection', ws => {
-	
-// // console.log("CONNECTED TO CLIENT!")
-//   // ws.send('Hello! Message From Server!!')
-
-//   ws.on('message', message => {
-//     console.log(`Received message => ${message}`)
-//     ws.send(JSON.stringify("here is the reply"))
-//   })
-
-//   ws.on("error", function(err){
-//   	console.error("error", err)
-//   })
-  
-// })
-
-// wss.on('open', function open() {
-//   console.log("oepned");
-//   wss.send('opened, something');
-// });
-
-
-// var ccxt = require("ccxt");
-
-// // (() => async function () {
-
-
-// //     let allMarkets = await binance.loadMarkets()
-
-// //     console.log(allMarkets)
-
-
-// // }) ()
-
-// let binance = new ccxt.binance()
-// var fs = require("fs");
-
-// // var data = "New File Contents";
-
-
-
-
-// var allMarkets;
-// var array;
-
-// var i
-
-// var start = async function (){
-
-// 	allMarkets = await binance.loadMarkets();
-// 	// console.log("I am allMarkets", Object.keys(allMarkets))
-
-// 	console.log("in the async", Object.keys(allMarkets))
-// 	// return Object.keys(allMarkets)
-// 	array = Object.keys(allMarkets);
-
-// 	fs.writeFile("temp.txt", array, (err) => {
-// 	  if (err) console.log(err);
-// 	  console.log("Successfully Written to File.");
-// 	});
-
-// };
-
-// start();
-
-// console.log("array", array);
-
-
